9f9dcf246bf715bcb3add1a7ae4631e0c3435488
Roy Dybing <roy.dybing@gmail.com>
2017-05-15 13:33:36 +0200

    Changed formatting

--- csKata/csKata.cs
+++ csKata/csKata.cs
@@ -2,59 +2,58 @@
 using static System.Console;
 
 namespace csKata {
-	class csKata {
-		static void Main(string[] args) {
-			string input = "";
-			bool quit = false;
 
-			help();
+class csKata {
+    static void Main(string[] args) {
+        string input = "";
+        bool quit = false;
 
-			while(quit == false){
-				Write("CS :> ");
-				input = ReadLine();
+        help();
 
-				switch (input) {
-					case "fizzbuzz":
-						FizzBuzz.Run();
-						break;
-					case "help":
-						help();
-						break;
-					case "quit":
-						quit = true;
-						break;
-				}
-			}
-		}
+        while(quit == false){
+            Write("CS :> ");
+            input = ReadLine();
 
+            switch (input) {
+            case "fizzbuzz":
+                FizzBuzz.Run();
+                break;
+            case "help":
+                help();
+                break;
+            case "quit":
+                quit = true;
+                break;
+            }
+        }
+    }
 
-
-		static void help() {
-			WriteLine("Available Commands");
-			WriteLine(" - fizzbuzz   | the drinking game");
-			//WriteLine(" - gcd        | find greatest common denominator");
-			//WriteLine(" - clock      | word clock");
-			WriteLine(" - help       | list of commands");
-			WriteLine(" - quit       | exit this application");
-		}
-	}
-
-	class FizzBuzz {
-		public static void Run() {
-			string text = "";
-
-			for(int i = 1; i <= 100; i++) {
-				if (i % 15 == 0) {
-					text = "FizzBuzz";
-				} else if (i % 5 == 0) {
-					text = "Fizz";
-				} else if (i % 3 == 0) {
-					text = "Buzz";
-				} else { 
-					text = i.ToString();
-				}
-				WriteLine("{0:D3} : {1}", i, text);
-			}
-		}
-	}
+    static void help() {
+        WriteLine("Available Commands");
+        WriteLine(" - fizzbuzz   | the drinking game");
+        //WriteLine(" - gcd        | find greatest common denominator");
+        //WriteLine(" - clock      | word clock");
+        WriteLine(" - help       | list of commands");
+        WriteLine(" - quit       | exit this application");
+        }
 }
+
+class FizzBuzz {
+    public static void Run() {
+        string text = "";
+
+        for(int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                text = "FizzBuzz";
+            } else if (i % 5 == 0) {
+                text = "Fizz";
+            } else if (i % 3 == 0) {
+                text = "Buzz";
+            } else { 
+                text = i.ToString();
+            }
+            WriteLine("{0:D3} : {1}", i, text);
+        }
+    }
+}
+} // end namespace
